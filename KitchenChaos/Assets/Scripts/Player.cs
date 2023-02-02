7b8f618222d80963d2db8868e539b53735e64e7a
Henrique Moura <henriq8383@gmail.com>
2023-02-02 19:04:23 -0300

    Moviment and Animations Idle and Walk

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
+
+    private void Update()
     {
-        
+        Vector2 InputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            InputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            InputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            InputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            InputVector.x = +1;
+        }
+        InputVector = InputVector.normalized;
+
+        Vector3 moveDir = new Vector3(InputVector.x, 0f, InputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsWalking()
     {
-        
+        return isWalking;
     }
 }
