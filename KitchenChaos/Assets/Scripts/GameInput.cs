007ebeaf2a64f00dfa44494b58663ba5462360a8
Mouque <henriq8383@gmail.com>
2023-02-03 10:47:44 -0300

    Refactoring to new input system

--- KitchenChaos/Assets/Scripts/GameInput.cs
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -4,25 +4,17 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
     public Vector2 GetMovimentVectorNormalized()
     {
-        Vector2 InputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            InputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            InputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            InputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            InputVector.x = +1;
-        }
+        Vector2 InputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
         InputVector = InputVector.normalized;
 
         return InputVector;
