af5c3ede0de91df9992ef927c74e62295f522cd0
Mouque <henriq8383@gmail.com>
2023-02-12 15:49:46 -0300

    Começo de scriptable objects

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
