4f7507e818b9c4e1790bf17fc1d9b73e2ed7b94b
Henrique Moura <henriq8383@gmail.com>
2023-03-21 18:44:23 -0300

    Plates Icons UI and DeliveryCounter Shader

--- KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
@@ -46,4 +46,9 @@ public class PlateKitchenObject : KitchenObject
         }
 
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 }
